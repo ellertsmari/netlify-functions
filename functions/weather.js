6708aa73307175d413739bd44f04fca6ba7a3136
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2023-11-01 15:02:01 +0000

    added weather api

---
+++ functions/weather.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import dotenv from 'dotenv'
+dotenv.config();
+
+exports.handler = async function(event, context) {
+  const key = process.env.WEATHER_API_KEY;
+  try {
+    const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=reykjavik&days=1&aqi=no&alerts=no`);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow any origin
+        "Access-Control-Allow-Methods": "GET, POST", // Allow GET and POST methods
+        "Content-Type": "application/json"
+    },
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "An error occurred fetching data" }),
+    };
+  }
+};
